7dd16e4d1d6c1c102a688709fb1d7e3309a6f68a
Esteban Z <zam22119@uvg.edu.gt>
2024-04-30 20:13:54 -0600

    feat: added footer model

---
+++ WebProyecto1/src/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Footer() {
+  const styles = {
+    padding: '20px 0',
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    color: '#fff',
+    boxShadow: '0 2px 4px rgba(0,0,0,0.3)',
+    textShadow: '1px 1px 2px black',
+    width: '100%',
+    textAlign: 'center',
+    bottom: 0,
+    position: 'fixed', 
+  };
+
+  return (
+    <footer style={styles}>
+      Derechos de autor 2024 UVG
+    </footer>
+  );
+}
