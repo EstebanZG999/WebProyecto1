fba0cc1019e7e102dfc29334622a6c15284d05a8
Esteban Z <zam22119@uvg.edu.gt>
2024-05-07 18:24:19 -0600

    feat: added token capabilities

--- WebProyecto1/api/api.js
+++ WebProyecto1/api/api.js
@@ -1,49 +1,43 @@
-// En tu archivo api.js
-
 const API_BASE_URL = 'https://api.tiburoncin.lat/22119/posts';
 
-export async function getPosts() {
-  const response = await fetch(`${API_BASE_URL}`);
+async function fetchWithAuth(url, options = {}) {
+  const token = localStorage.getItem('token');
+  const headers = {
+    'Content-Type': 'application/json',
+    ...options.headers,
+  };
+
+  if (token) {
+    headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  const response = await fetch(url, { ...options, headers });
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
   return response.json();
 }
 
-export async function createPost(postData) {
-  const response = await fetch(`${API_BASE_URL}`, {
+export const getPosts = async () => {
+  return fetchWithAuth(`${API_BASE_URL}`);
+};
+
+export const createPost = async (postData) => {
+  return fetchWithAuth(`${API_BASE_URL}`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify(postData),
   });
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
-}
+};
 
-export async function updatePost(id, postData) {
-  const response = await fetch(`${API_BASE_URL}/${id}`, {
+export const updatePost = async (id, postData) => {
+  return fetchWithAuth(`${API_BASE_URL}/${id}`, {
     method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify(postData),
   });
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
-}
+};
 
-export async function deletePost(id) {
-  const response = await fetch(`${API_BASE_URL}/${id}`, {
+export const deletePost = async (id) => {
+  return fetchWithAuth(`${API_BASE_URL}/${id}`, {
     method: 'DELETE',
   });
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
-}
+};
